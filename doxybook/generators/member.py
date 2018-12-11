6a7dd97f3662bad6489e75b845f2037d89cc4db5
Andrianto Lie <andrianto.lie@ii5.com>
2018-12-11 16:14:18 +0800

    Use declname instead of defname

--- doxybook/generators/member.py
+++ doxybook/generators/member.py
@@ -156,9 +156,9 @@ def make_function_code(compoundname: str, memberdef: xml.etree.ElementTree.Eleme
         templateparams_code = 'template<'
         for index, templateparam in enumerate(templateparams):
             templateparams_code += templateparam.findtext('type')
-            templateparam_defname = templateparam.findtext('defname')
-            if templateparam_defname:
-                templateparams_code += ' ' + templateparam_defname
+            templateparam_declname = templateparam.findtext('declname')
+            if templateparam_declname:
+                templateparams_code += ' ' + templateparam_declname
             if index != len(templateparams) - 1:
                 templateparams_code += ', '
             else:
